bf8a21929b22ef16eb7afddc56633e4f8820e94c
Jeremy Tregunna <jeremy@tregunna.ca>
2013-05-21 19:11:33 -0600

    imports cleaned up source code

--- JDataStack/JDataStack.h
+++ JDataStack/JDataStack.h
@@ -1,13 +1,37 @@
 //
 //  JDataStack.h
-//  JDataStack
+//  JKit
 //
-//  Created by Jeremy Tregunna on 2013-05-21.
-//  Copyright (c) 2013 Jeremy Tregunna. All rights reserved.
+//  Created by Jeremy Tregunna on 2012-12-30.
+//  Copyright (c) 2012 Jeremy Tregunna. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@protocol JDataStackDelegate;
 
 @interface JDataStack : NSObject
+@property (nonatomic, readonly) NSManagedObjectContext* mainManagedObjectContext;
+@property (nonatomic, readonly) NSManagedObjectContext* writeManagedObjectContext;
 
+@property (nonatomic, readonly) NSPersistentStoreCoordinator* persistentStoreCoordinator;
+@property (nonatomic, readonly) NSManagedObjectModel* managedObjectModel;
+
+- (instancetype)initWithModelName:(NSString*)modelName delegate:(id<JDataStackDelegate>)delegate;
+
+- (NSManagedObjectContext*)newPrivateManagedObjectContext;
+
+- (void)saveWriteContext;
+- (void)saveMainContext;
+- (void)saveContext:(NSManagedObjectContext*)context;
+- (void)writeToDisk;
+- (void)performBlock:(void (^)())block target:(id)target async:(BOOL)async;
+
+@end
+
+@protocol JDataStackDelegate <NSObject>
+@optional
+- (void)dataStack:(JDataStack*)dataStack willSaveWithSaveContext:(NSManagedObjectContext*)saveContext;
+- (void)dataStack:(JDataStack*)dataStack didSaveWithSaveContext:(NSManagedObjectContext*)saveContext;
 @end
--- JDataStack/JDataStack.m
+++ JDataStack/JDataStack.m
@@ -1,13 +1,227 @@
 //
 //  JDataStack.m
-//  JDataStack
+//  JKit
 //
-//  Created by Jeremy Tregunna on 2013-05-21.
-//  Copyright (c) 2013 Jeremy Tregunna. All rights reserved.
+//  Created by Jeremy Tregunna on 2012-12-30.
+//  Copyright (c) 2012 Jeremy Tregunna. All rights reserved.
 //
 
 #import "JDataStack.h"
 
+@interface JDataStack ()
+@property (nonatomic, strong) id<JDataStackDelegate> delegate;
+@property (nonatomic, copy) NSString* modelName;
+- (NSURL*)applicationDocumentsDirectory;
+@end
+
 @implementation JDataStack
+
+@synthesize delegate = _delegate;
+@synthesize modelName = _modelName;
+@synthesize mainManagedObjectContext = _mainManagedObjectContext;
+@synthesize persistentStoreCoordinator = _persistentStoreCoordinator;
+@synthesize managedObjectModel = _managedObjectModel;
+@synthesize writeManagedObjectContext = _writeManagedObjectContext;
+
+- (instancetype)initWithModelName:(NSString*)modelName delegate:(id<JDataStackDelegate>)delegate
+{
+    if((self = [super init]))
+    {
+        NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
+        [center addObserver:self selector:@selector(_willSave:) name:NSManagedObjectContextWillSaveNotification object:nil];
+        [center addObserver:self selector:@selector(_didSave:) name:NSManagedObjectContextDidSaveNotification object:nil];
+
+        self.modelName = modelName;
+        self.delegate = delegate;
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    self.delegate = nil;
+}
+
+#pragma mark - Core Data Methods
+
+- (void)writeToDisk
+{
+    NSManagedObjectContext* writeManagedObjectContext = [self writeManagedObjectContext];
+    NSManagedObjectContext* mainManagedObjectContext = [self mainManagedObjectContext];
+
+    [mainManagedObjectContext performBlock:^{
+        NSError* error = nil;
+        if([mainManagedObjectContext hasChanges] && ![mainManagedObjectContext save:&error])
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+
+        [writeManagedObjectContext performBlock:^{
+            NSError* error = nil;
+            if([writeManagedObjectContext hasChanges] && ![writeManagedObjectContext save:&error])
+                NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+        }];
+    }];
+}
+
+- (void)performBlock:(void (^)())block target:(id)target async:(BOOL)async
+{
+    if (async)
+        [target performBlock:block];
+    else
+        [target performBlockAndWait:block];
+}
+
+- (void)saveWriteContext
+{
+    NSManagedObjectContext* managedObjectContext = [self writeManagedObjectContext];
+    [self saveContext:managedObjectContext];
+}
+
+
+- (void)saveMainContext
+{
+    NSManagedObjectContext* managedObjectContext = [self mainManagedObjectContext];
+    [self saveContext:managedObjectContext];
+}
+
+- (void)saveContext:(NSManagedObjectContext*)context
+{
+    // You need the performBlock to execute the save on the Context Queue
+    void (^block)() = ^{
+        NSError* error = nil;
+        if([context hasChanges] && ![context save:&error])
+        {
+            NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+            abort();
+        }
+        
+    };
+
+    [self performBlock:block target:context async:YES];
+}
+
+#pragma mark - Core Data stack
+
+- (NSManagedObjectContext*)newPrivateManagedObjectContext
+{
+    NSManagedObjectContext* privateManagedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSPrivateQueueConcurrencyType];
+    [privateManagedObjectContext setParentContext:[self mainManagedObjectContext]];
+    [privateManagedObjectContext setUndoManager:nil];
+    return privateManagedObjectContext;
+}
+
+- (NSManagedObjectContext*)writeManagedObjectContext
+{
+    @synchronized(self)
+    {
+        if(_writeManagedObjectContext != nil)
+            return _writeManagedObjectContext;
+
+        NSPersistentStoreCoordinator* coordinator = [self persistentStoreCoordinator];
+
+        if(coordinator != nil)
+        {
+            _writeManagedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType: NSPrivateQueueConcurrencyType];
+            [_writeManagedObjectContext setUndoManager:nil];
+            [_writeManagedObjectContext setPersistentStoreCoordinator:coordinator];
+        }
+
+        return _writeManagedObjectContext;
+    }
+}
+
+- (NSManagedObjectContext*)mainManagedObjectContext
+{
+    @synchronized(self)
+    {
+        if(_mainManagedObjectContext != nil)
+            return _mainManagedObjectContext;
+
+        _mainManagedObjectContext = [[NSManagedObjectContext alloc] initWithConcurrencyType:NSMainQueueConcurrencyType];
+        [_mainManagedObjectContext setParentContext:[self writeManagedObjectContext]];
+
+        return _mainManagedObjectContext;
+    }
+}
+
+- (NSManagedObjectModel*)managedObjectModel
+{
+    @synchronized(self)
+    {
+        if(_managedObjectModel != nil)
+            return _managedObjectModel;
+
+        NSURL* modelURL = [[NSBundle mainBundle] URLForResource:_modelName withExtension:@"momd"];
+        _managedObjectModel = [[NSManagedObjectModel alloc] initWithContentsOfURL:modelURL];
+        return _managedObjectModel;
+    }
+}
+
+- (NSPersistentStoreCoordinator*)persistentStoreCoordinator
+{
+    @synchronized(self)
+    {
+        if(_persistentStoreCoordinator != nil)
+            return _persistentStoreCoordinator;
+        NSURL* storeURL = [[self applicationDocumentsDirectory] URLByAppendingPathComponent:[_modelName stringByAppendingString:@".sqlite"]];
+
+        NSError* error = nil;
+        _persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:[self managedObjectModel]];
+        if(![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error])
+        {
+            [[NSFileManager defaultManager] removeItemAtURL:storeURL error:nil];
+            if(![_persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType configuration:nil URL:storeURL options:nil error:&error])
+            {
+                NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
+                abort();
+            }
+        }
+
+        NSDictionary* fileAttributes = [NSDictionary dictionaryWithObject:NSFileProtectionComplete forKey:NSFileProtectionKey];
+        if(![[NSFileManager defaultManager] setAttributes:fileAttributes ofItemAtPath:[storeURL path] error:&error])
+        {
+#ifdef DEBUG
+            NSLog(@"Unable to set file protection attribute on store. Error: %@", error);
+#endif
+        }
+        return _persistentStoreCoordinator;
+    }
+}
+
+#pragma mark - Notifications
+
+- (void)_willSave:(NSNotification*)notification
+{
+    if([_delegate respondsToSelector:@selector(dataStack:willSaveWithSaveContext:)])
+        [_delegate dataStack:self willSaveWithSaveContext:notification.object];
+}
+
+- (void)_didSave:(NSNotification*)notification
+{
+    NSManagedObjectContext* saveContext = notification.object;
+    NSManagedObjectContext* writeContext = self.writeManagedObjectContext;
+
+    if(saveContext == writeContext)
+        return;
+
+    if(saveContext.persistentStoreCoordinator && (writeContext.persistentStoreCoordinator != saveContext.persistentStoreCoordinator))
+        return;
+
+    [saveContext performBlock:^{
+        [saveContext.parentContext performBlock:^{
+            [saveContext.parentContext mergeChangesFromContextDidSaveNotification:notification];
+
+            if([_delegate respondsToSelector:@selector(dataStack:didSaveWithSaveContext:)])
+                [_delegate dataStack:self didSaveWithSaveContext:saveContext];
+        }];
+    }];
+}
+
+#pragma mark - Application's Documents directory
+
+- (NSURL*)applicationDocumentsDirectory
+{
+    return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+}
 
 @end
