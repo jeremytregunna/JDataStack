54da87f4aa590900b2b22d87bc9664b4d734f2b8
Jeremy Tregunna <jeremy@tregunna.ca>
2013-05-21 19:10:59 -0600

    Initial Commit

---
+++ JDataStack/JDataStack.h
@@ -0,0 +1,13 @@
+//
+//  JDataStack.h
+//  JDataStack
+//
+//  Created by Jeremy Tregunna on 2013-05-21.
+//  Copyright (c) 2013 Jeremy Tregunna. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JDataStack : NSObject
+
+@end
---
+++ JDataStack/JDataStack.m
@@ -0,0 +1,13 @@
+//
+//  JDataStack.m
+//  JDataStack
+//
+//  Created by Jeremy Tregunna on 2013-05-21.
+//  Copyright (c) 2013 Jeremy Tregunna. All rights reserved.
+//
+
+#import "JDataStack.h"
+
+@implementation JDataStack
+
+@end
